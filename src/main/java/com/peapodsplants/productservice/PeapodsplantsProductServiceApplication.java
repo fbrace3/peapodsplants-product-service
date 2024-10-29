b0beba97f60a55e708cd48f4edbc16d11cc64aa5
fbrace3 <fbrace3@msn.com>
2024-10-29 10:39:12 -0600

    Initial setup for Product Service

---
+++ src/main/java/com/peapodsplants/productservice/PeapodsplantsProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.peapodsplants.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeapodsplantsProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeapodsplantsProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peapodsplants/productservice/PeapodsplantsProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peapodsplants.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeapodsplantsProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
